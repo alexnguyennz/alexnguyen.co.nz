e1c0cfd0f1b5406b7e15d1d9c3c865b26cb01e01
Alex <dev@alexnguyen.co.nz>
2023-10-09 18:59:19 +1300

    update dependencies

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				sans: ["Jost", "system-ui"],
-			},
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Jost", "system-ui"],
+      },
+    },
+  },
+  plugins: [],
+};

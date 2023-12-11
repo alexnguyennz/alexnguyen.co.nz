58f4e2d9540963b79955796fe027615fc4ea14a6
Alex Nguyen <dev@alexnguyen.co.nz>
2023-12-12 10:44:56 +1300

    voice integration (#51)
    - initial but disabled

---
+++ src/content/pages/about.md
@@ -0,0 +1,13 @@
+---
+
+---
+
+Hi there, I'm a web developer from New Zealand. After studying to be a
+network engineer and working in IT support related roles, I decided to
+become a developer in 2023.
+
+In my spare time, I like to read (slowly), run on local tracks, watch
+movies and shows, and play the piano (poorly). I also watch too much
+basketball.
+
+This site is built with Astro and Tailwind CSS. Hosted on Netlify. Source code is available here.
---
+++ src/pages/api/voice.ts
@@ -0,0 +1,19 @@
+import type { APIRoute } from "astro";
+
+import ElevenLabs from "elevenlabs-node";
+
+const voice = new ElevenLabs({
+  apiKey: import.meta.env.XI_API_KEY,
+  voiceId: import.meta.env.XI_VOICE_ID,
+});
+
+export const POST: APIRoute = async ({ request }) => {
+  const { id, textInput } = await request.json();
+
+  const response = await voice.textToSpeech({
+    fileName: `public/voice/${id}.mp3`,
+    textInput,
+  });
+
+  return new Response(JSON.stringify(response));
+};

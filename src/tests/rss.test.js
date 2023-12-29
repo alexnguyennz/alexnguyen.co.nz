54f5a3716b521da989dab007cfcd0b5bc7aa8edb
alexnguyennz <dev@alexnguyen.co.nz>
2023-12-30 11:36:06 +1300

    unit tests

---
+++ src/tests/rss.test.js
@@ -0,0 +1,13 @@
+import { GET } from "@/pages/rss.xml.js";
+import config from "/astro.config.mjs";
+
+test("returns RSS XML", async () => {
+  const response = await GET({ site: config.site });
+  const data = await response.text();
+
+  expect(data)
+    .to.be.a("string")
+    .and.satisfy((msg) =>
+      msg.startsWith('<?xml version="1.0" encoding="UTF-8"?>'),
+    );
+});

f89220a816db80ba16167e1afa4ef5edb31a192d
Alex <dev@alexnguyen.co.nz>
2024-01-26 07:10:40 +1300

    re-organized tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:4321",
+    specPattern: "src/tests/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    supportFile: "src/tests/e2e/support.ts",
     fixturesFolder: false,
     screenshotOnRunFailure: false,
     video: false,
